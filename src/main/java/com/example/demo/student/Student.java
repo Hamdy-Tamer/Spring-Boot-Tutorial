0633ef09896fc395568a8d032f87ee2a270b07a2
Hamdy-Tamer <hamdytamer253@gmail.com>
2026-06-28 13:31:04 +0300

    Refactor Student class to simplify code
    Removed unused constructors and getters/setters. Added computeAge method to calculate age based on date of birth.

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,10 +1,18 @@
 package com.example.demo.student;
 
+
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.time.Period;
 
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 @Entity
 @Table
 public class Student {
@@ -18,6 +26,7 @@ public class Student {
             strategy = GenerationType.SEQUENCE,
             generator = "student_sequence"
     )
+
     private Long id;
     private String name;
     private String email;
@@ -25,71 +34,16 @@ public class Student {
     @Transient
     private Integer age;
 
-    public Student(){
-
-    }
-
-    /*public Student(Long id, String name, String email, LocalDate dob){
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.dob = dob;
-    }*/
-
     public Student(String name, String email, LocalDate dob){
         this.name = name;
         this.email = email;
         this.dob = dob;
     }
 
-    public Long getId() {
-        return id;
-    }
+    public Integer computeAge(){
+        return Period
+                .between(this.dob, LocalDate.now())
+                .getYears();
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDate getDob() {
-        return dob;
-    }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
-
-    public Integer getAge() {
-        return Period.between(this.dob, LocalDate.now()).getYears();
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", dob=" + dob +
-                ", age=" + age +
-                '}';
     }
 }
