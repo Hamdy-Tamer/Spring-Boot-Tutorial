fe869099b636e6fe723d6c7af39fc3181237520b
Hamdy-Tamer <hamdytamer253@gmail.com>
2026-06-28 16:14:21 +0300

    Refactor CourseController for DTO usage
    Updated CourseController to use request and response DTOs for course operations.

--- src/main/java/com/example/demo/course/CourseController.java
+++ src/main/java/com/example/demo/course/CourseController.java
@@ -1,44 +1,63 @@
 package com.example.demo.course;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
-@RequestMapping(path = "api/v1/courses")
+@RequestMapping(path = "/api/v1/courses")
 public class CourseController {
+
     private final CourseService courseService;
+
 
     @Autowired
     public CourseController(CourseService courseService){
         this.courseService = courseService;
     }
 
+
     @GetMapping
-    public List<Course> getCourses(){
+    public List<CourseResponse> getCourses(){
         return courseService.getCourses();
     }
 
+
     @PostMapping
-    public void registerNewCourse(@RequestBody Course course){
-        courseService.addNewCourse(course);
+    public CourseResponse registerNewCourse(
+            @RequestBody @Valid CourseRequest request){
+
+        return courseService.addNewCourse(request);
     }
 
-    @DeleteMapping(path = "{courseID}")
-    public void deleteCourse(@PathVariable("courseID") int courseID){
+
+    @DeleteMapping("/{courseID}")
+    public void deleteCourse(@PathVariable Long courseID){
+
         courseService.deleteCourse(courseID);
     }
 
-    // PUT
-    @PutMapping(path = "{courseID}")
-    public void updateCourse(@PathVariable("courseID") int courseID, @RequestBody Course course){
-        courseService.updateCourse(courseID, course);
+
+
+    @PutMapping("/{courseID}")
+    public CourseResponse updateCourse(
+            @PathVariable Long courseID,
+            @RequestBody @Valid CourseRequest request){
+
+        return courseService.updateCourse(courseID, request);
     }
 
+
+
     // PATCH
-    @PatchMapping(path = "{courseID}")
-    public void updateCoursePatch(@PathVariable int courseID, @RequestBody Course course){
-        courseService.patchCourse(courseID, course);
+    @PatchMapping("/{courseID}")
+    public CourseResponse patchCourse(
+            @PathVariable Long courseID,
+            @RequestBody CoursePatchRequest request){
+
+        return courseService.patchCourse(courseID, request);
     }
 }
